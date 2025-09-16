cdf209b41dde30ce5254c989d87f7045829ff4e5
LorenzoGarcia10 <lorenzogdm0@gmail.com>
2025-09-16 17:22:17 -0400

    .env

--- lib/axiosInstance.ts
+++ lib/axiosInstance.ts
@@ -1,10 +1,25 @@
-import axios from 'axios';
+import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("authToken");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
